13dbf339bfb5dc3ee2bd29bbb4ffda1406caf3cb
DESKTOP-UVRA2QD\peachman <patcharapon1995@gmail.com>
2017-10-22 06:01:01 -0700

    can edit address

--- mysite/data/views.py
+++ mysite/data/views.py
@@ -1,34 +1,56 @@
 from django.shortcuts import render
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect ,get_object_or_404
+from django.http import Http404
 from .models import *
 # Create your views here.
 def personal_info(request):
-	return render(request,'data/personal_info.html', {})
+    return render(request,'data/personal_info.html', {})
 
 def address(request):
-    form = AddressForm()
-    return render(request,'data/address.html', {'form':form})
+    if request.user.is_authenticated:        
+        print(request.user.is_staff)
+
+        try: # edit
+            addressObj = Address.objects.get(user=request.user.id)
+            form = AddressForm(request.POST or None, instance=addressObj)
+        except MyModel.DoesNotExist: # create
+            form = AddressForm()
+
+        if request.method == 'POST' and form.is_valid():
+            form.save()
+            return redirect('home')
+        
+        return render(request,'data/address.html', {'form':form})
+    else:
+        return render(request,'home.html', {})
 
 def work_info(request):
-	return render(request,'data/work_info.html', {})
+    return render(request,'data/work_info.html', {})
 
 def insignia(request):
-	return render(request,'data/insignia.html', {})
+    return render(request,'data/insignia.html', {})
 
 def education(request):
-    if request.method == 'POST':
-        form = ChoiceForm(request.POST)		
-        if form.is_valid():
-            print(form)
-            form.save()
-            # username = form.cleaned_data.get('username')
-            # raw_password = form.cleaned_data.get('password1')
-            # user = authenticate(username=username, password=raw_password)
-            # login(request, user)
-            # return render(request, 'home.html')
-            return redirect('home')
+    # choiceObject = get_object_or_404(Choice, choice_text="1234")
+    print(request.user.id)
+    print(get_object_or_404(Address, user=request.user.id))
+    if request.method == 'POST' and form.is_valid():
+        print("in")
+        article = get_object_or_404(Choice, choice_text="1234")
+        form = ChoiceForm(request.POST, instance=article)		
+        
+        print(form)
+        form.save()
+        return redirect('home')
     else:
         # form = ChoiceForm()
+        print("else")
         article = get_object_or_404(Choice, choice_text="1234")
-        form = ChoiceForm(request.POST or None,instance=article)    
+        form = ChoiceForm(request.POST or None,instance=article)
+        # form.votes = 9
+        # form = ChoiceForm()
+        # choiceEn = form.save(commit=False)
+        # choiceEn.votes = 9
+        # print(choiceEn.votes)    
+        # print(form)
     return render(request,'data/education.html', {'form':form})
